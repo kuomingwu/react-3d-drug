8df2b3da1b641f99e33697e532b1de36d3caef31
吳國銘 <kuomingwu@gmail.com>
2020-08-02 12:04:49 +0800

    Updates

---
+++ precache-manifest.11d44257e6218cc2c31033167127628e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e21a46e0da1921eeb72e034045ccd00",
+    "url": "/react-3d-drug/index.html"
+  },
+  {
+    "revision": "56e5582f8cd12f072ba6",
+    "url": "/react-3d-drug/static/css/main.34d95db0.chunk.css"
+  },
+  {
+    "revision": "78ca0b02f0a4d14a85ff",
+    "url": "/react-3d-drug/static/js/2.1963d1ba.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/react-3d-drug/static/js/2.1963d1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e5582f8cd12f072ba6",
+    "url": "/react-3d-drug/static/js/main.a8aade18.chunk.js"
+  },
+  {
+    "revision": "70dcceffec7bee29fbf9",
+    "url": "/react-3d-drug/static/js/runtime-main.bcb72838.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-3d-drug/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f54c2b3cfdfd66a51e64e2a661546c44.js"
+  "/react-3d-drug/precache-manifest.11d44257e6218cc2c31033167127628e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-3d-drug/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
