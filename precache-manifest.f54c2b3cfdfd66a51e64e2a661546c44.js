cf70e4728085fa6fb282530e1730da9f0d065ff3
吳國銘 <kuomingwu@gmail.com>
2020-08-02 12:03:35 +0800

    Updates

---
+++ precache-manifest.f54c2b3cfdfd66a51e64e2a661546c44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ab9a10f51a5f0747afaf391bb4e546",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4650f8d90272116399cb",
+    "url": "/static/css/main.34d95db0.chunk.css"
+  },
+  {
+    "revision": "78ca0b02f0a4d14a85ff",
+    "url": "/static/js/2.1963d1ba.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.1963d1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4650f8d90272116399cb",
+    "url": "/static/js/main.5fe47c4f.chunk.js"
+  },
+  {
+    "revision": "5fa1c6b578a3af8c52e2",
+    "url": "/static/js/runtime-main.33daf755.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
